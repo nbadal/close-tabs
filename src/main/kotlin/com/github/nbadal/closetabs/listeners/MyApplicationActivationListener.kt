1ef3fef829c10f6ab97601e2900fe1d5d5997d34
GitHub Action <action@github.com>
2024-01-31 05:23:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/nbadal/closetabs/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nbadal/closetabs/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nbadal.closetabs.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
